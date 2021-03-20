eaedbc078e65fd6e15e042c15a6f6916088a7cf7
ScottS2017 <scottstoll2017@gmail.com>
2021-03-20 18:35:10 -0400

    Refactored app strings class into a map

--- lib/app/app_strings.dart
+++ lib/app/app_strings.dart
@@ -1,34 +1,23 @@
-class AppStrings {
-  static const String firebaseLoginErrorMessage = 'We\'re sorry, there was an error.\nPlease try again later.';
-  static const String login = 'Log in';
-  static const String signOut = 'Sign out';
-  static const String signUp = 'Sign up';
-
-  static const List<List<String>> testString = [
-    ['test', 'test'],
-    ['two']
-  ];
-
-  // Strings used in this app
-  static const String AAVE = 'Aave';
-  static const String ALGO = 'ALGO';
-  static const String BCH = 'Bitcoin Cash';
-  static const String BTC = 'Bitcoin';
-  static const String BUSD = 'Binance USD';
-  static const String DGLD = 'DGLD';
-  static const String ENJ = 'Enjin Coin';
-  static const String ETH = 'Ethereum';
-  static const String EUR = 'Euros';
-  static const String GBP = 'British Pounds';
-  static const String LEND = 'Aave Coin';
-  static const String LTC = 'Litecoin';
-  static const String OGN = 'Origin Protocol';
-  static const String PAX = 'Paxos Standard';
-  static const String TRY = 'Trias';
-  static const String USD = 'US Dollars';
-  static const String USDT = 'Tether';
-  static const String WDGLD = 'wrapped-DGLD';
-  static const String XLM = 'Stellar';
-  static const String XRP = 'XRP';
-  static const String YFI = 'yearn.finance';
-}
+Map<String, String> appStrings  = {
+   'AAVE' : 'Aave',
+   'ALGO' : 'ALGO',
+   'BCH' : 'Bitcoin Cash',
+   'BTC' : 'Bitcoin',
+   'BUSD' : 'Binance USD',
+   'DGLD' : 'DGLD',
+   'ENJ' : 'Enjin Coin',
+   'ETH' : 'Ethereum',
+   'EUR' : 'Euros',
+   'GBP' : 'British Pounds',
+   'LEND' : 'Aave Coin',
+   'LTC' : 'Litecoin',
+   'OGN' : 'Origin Protocol',
+   'PAX' : 'Paxos Standard',
+   'TRY' : 'Trias',
+   'USD' : 'US Dollars',
+   'USDT' : 'Tether',
+   'WDGLD' : 'wrapped-DGLD',
+   'XLM' : 'Stellar',
+   'XRP' : 'XRP',
+   'YFI' : 'yearn.finance',
+};
